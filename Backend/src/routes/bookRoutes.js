bb115495b411355e387dd5047134e035fe882116
Jo <jo-dvplr@gmail.com>
2025-02-16 15:00:25 +0100

    add-ajout des fichiers pour gérer l authentification

--- Backend/src/routes/bookRoutes.js
+++ Backend/src/routes/bookRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express')
-const router = express.Router()
 const bookCtrl = require('../controllers/bookCtrl')
+const auth = require('../middleware/auth')
+const router = express.Router()
 
 router.get('/', bookCtrl.getAllBooks)
 router.get('/bestrating', bookCtrl.bestRatedBooks) //⚠️ A placer avant /books/:id sinon => interpréter comme un id
 router.get('/:id', bookCtrl.getOneBook)
-router.post('/', bookCtrl.createBook)
-router.post('/:id/rating', bookCtrl.ratingBook)
-router.put('/:id', bookCtrl.updateBook)
-router.delete('/:id', bookCtrl.deleteBook)
+router.post('/', auth, bookCtrl.createBook)
+router.post('/:id/rating', auth, bookCtrl.ratingBook)
+router.put('/:id', auth, bookCtrl.updateBook)
+router.delete('/:id', auth, bookCtrl.deleteBook)
 
 module.exports = router
